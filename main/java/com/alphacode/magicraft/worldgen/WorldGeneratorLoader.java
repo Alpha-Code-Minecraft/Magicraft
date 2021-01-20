ca4569e53994e114b661abba61f806e55efef73f
Jason-Alpha <64051651+JasonAlpha-MODDER@users.noreply.github.com>
2021-01-20 17:00:38 +0800

    Create WorldGeneratorLoader.java

---
+++ main/java/com/alphacode/magicraft/worldgen/WorldGeneratorLoader.java
@@ -0,0 +1,20 @@
+package com.github.alphacode.magicraft.worldgen;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.terraingen.OreGenEvent;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+public class WorldGeneratorLoader {
+    public WorldGeneratorLoader()
+{
+    MinecraftForge.ORE_GEN_BUS.register(this);
+}
+
+    @SubscribeEvent
+    public void onOreGenGenerateMinable(OreGenEvent.GenerateMinable event)
+    {
+        if (event.getType() == OreGenEvent.GenerateMinable.EventType.ANDESITE)
+        {
+            event.setResult(Event.Result.DENY);
+        }
+    }
+}
