5f92195844d52b5afee9b33d2126f4b1a0ca1500
Jason-Alpha <64051651+JasonAlpha-MODDER@users.noreply.github.com>
2021-01-29 11:35:17 +0800

    Add files via upload

---
+++ main/java/com/alphacode/magicraft/worldgen/ore/SilverOreGen.java
@@ -0,0 +1,31 @@
+package com.github.alphacode.magicraft.worldgen.ore;
+import com.github.alphacode.magicraft.register.BlocksRegister;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraftforge.fml.common.IWorldGenerator;
+import java.util.Random;
+
+
+public class SilverOreGen implements IWorldGenerator {
+
+    private final WorldGenMinable WorldGenMinable;
+
+
+    public SilverOreGen(WorldGenMinable WorldGenMinable)
+    {
+        this.WorldGenMinable =new WorldGenMinable(BlocksRegister.SILVER_ORE.getDefaultState(), 5);
+    }
+
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+        for(int i = 10; i < 100; i *= 10) {
+            BlockPos pos = new BlockPos(chunkX * 16, i, chunkZ * 16);
+            WorldGenMinable.generate(world, random, pos);
+        }
+    }
+}
+
+
