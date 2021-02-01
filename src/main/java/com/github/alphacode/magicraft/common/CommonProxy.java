dacc0d1a839fe94b7872fadfd572ad7e28145732
Jason-Alpha <64051651+JasonAlpha-MODDER@users.noreply.github.com>
2021-02-01 11:46:11 +0800

    Update CommonProxy.java

--- src/main/java/com/github/alphacode/magicraft/common/CommonProxy.java
+++ src/main/java/com/github/alphacode/magicraft/common/CommonProxy.java
@@ -1,12 +1,13 @@
 package com.github.alphacode.magicraft.common;
 import com.github.alphacode.magicraft.register.BlocksRegister;
+import com.github.alphacode.magicraft.register.ItemsRegister;
 import  net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class CommonProxy {
     public void preInit(FMLPreInitializationEvent event)
-{
+{   new ItemsRegister(event);
     new BlocksRegister(event);
 }
 
