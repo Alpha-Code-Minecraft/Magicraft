22ca5132c6973c5caa9268da6f16084bc1142102
Jason-Alpha <64051651+JasonAlpha-MODDER@users.noreply.github.com>
2021-02-01 16:22:25 +0800

    Update Magicraft.java

--- src/main/java/com/github/alphacode/magicraft/Magicraft.java
+++ src/main/java/com/github/alphacode/magicraft/Magicraft.java
@@ -10,22 +10,12 @@ import com.github.alphacode.magicraft.common.CommonProxy;
 /**
  * @author AlphaCode:JasonAlpha
  */
-@Mod(modid = Magicraft.MODID, name = Magicraft.NAME, version = Magicraft.VERSION, acceptedMinecraftVersions = "1.12.2")
+@Mod(modid = Magicraft.MODID, name = Magicraft.NAME, version = "0.0.1", acceptedMinecraftVersions = "1.12.2",dependencies = "patchouli,baubles")
 public class Magicraft {
     public static final String MODID = "magicraft";
     public static final String NAME = "Magicraft";
-    public static final String VERSION = "Pre-Alpha0.0.1";
-//注册方块
-    @Instance(Magicraft.MODID)
-    public static Magicraft instance;
-    public static Object copperore;
-    public static Object cinnabarore;
-    public static Object leadore;
-    public static Object saltore;
-    public static Object silverore;
-    public static Object tinore;
-
-
+    public static final String VERSION = "0.0.1";
+    
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
